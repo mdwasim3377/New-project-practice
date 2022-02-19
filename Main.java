e9b639c8a9a72c4ca467a21a28d53cac4cc47cd9
Md Wasim Akram <86349842+wasim1akram@users.noreply.github.com>
2022-02-19 12:26:23 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+package hospital;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ doctor.java
@@ -0,0 +1,7 @@
+package hospital;
+
+public class doctor extends person {
+    String department, degree;
+
+
+}
